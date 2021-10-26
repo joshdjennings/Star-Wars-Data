8e6f94e8f00f60433f2b7971d56c612a2fd43b8d
joshua david jennings <jennings.josh@gmail.com>
2021-10-25 22:37:20 -0500

    Added dropdown button

---
+++ index.js
@@ -0,0 +1,9 @@
+//Event Listener 1 - hit a button and Fetches from Star Wars API
+
+// display Planets, Spaceships, Vehicles, People, Films and Species on screen and include a heart button to save it
+
+//Event Listener 2 - user can click the heart button and the information about that item will save down below
+
+//Event Listener 3 - delete option for saved info
+
+//stretch goal - add to db.json server to save the favorited characters
