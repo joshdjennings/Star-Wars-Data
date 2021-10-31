4f9eee6781484c2934c532f3a4c56d9c62773c75
joshua david jennings <jennings.josh@gmail.com>
2021-10-30 19:02:42 -0500

    reordered buttons from dropdown menu and console logged fetches

--- index.js
+++ index.js
@@ -1,35 +1,60 @@
 console.log ("Hi Star Wars Fans.  You can't see this because you're not a console.")
 
-//Event Listener 1 - hit a button and Fetches from Star Wars API
-document.getElementById("planets").addEventListener("click", clickPlanets);
-function clickPlanets() {
-  alert ("I have the high ground Anakin!");
-}
+//Event Listener 1 - hit a button and Fetches from Star Wars API https://www.swapi.tech/
+document.getElementById("planets").addEventListener("click", fetchPlanet);
+function fetchPlanet() {
+    fetch('https://www.swapi.tech/api/planets/')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err)
+    )
+  }
 
-document.getElementById("spaceships").addEventListener("click", clickShips);
-function clickShips() {
-  alert ("I have a BAD FEELING about this!");
-}
 
-document.getElementById("vehicles").addEventListener("click", clickVehicles);
-function clickVehicles() {
-  alert ("I am your FATHER!");
-}
+document.getElementById("starships").addEventListener("click", fetchShip);
+function fetchShip() {
+    fetch('https://www.swapi.tech/api/starships/')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err)
+    )
+  }
 
-document.getElementById("people").addEventListener("click", clickPeople);
-function clickPeople() {
-  alert ("You were the Chosen one!");
-}
+document.getElementById("vehicles").addEventListener("click", fetchVehicles);
+function fetchVehicles() {
+    fetch('https://www.swapi.tech/api/vehicles/')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err)
+    )
+  }
 
-document.getElementById("films").addEventListener("click", clickFilms);
-function clickFilms() {
-  alert ("Now feel the POWAH of the DARK SIDE!");
-}
+document.getElementById("people").addEventListener("click", fetchPeople);
+function fetchPeople() {
+    fetch('https://www.swapi.tech/api/people/')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err)
+    )
+  }
 
-document.getElementById("species").addEventListener("click", clickSpecies);
-function clickSpecies() {
-  alert ("May the FORCE be with you.");
-}
+document.getElementById("films").addEventListener("click", fetchFilms);
+function fetchFilms() {
+    fetch('https://www.swapi.tech/api/films/')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err)
+    )
+  }
+
+document.getElementById("species").addEventListener("click", fetchSpecies);
+function fetchSpecies() {
+    fetch('https://www.swapi.tech/api/species/')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err)
+    )
+  }
 
 // display info about selected random content on screen 
 
