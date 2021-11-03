ea5a21e73114d8e9debfcf5e3e6246d5c876f229
joshua david jennings <jennings.josh@gmail.com>
2021-11-03 00:40:13 -0500

    Cleaned up code and deleted unnecessary components

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Star-Wars-Data
+Your app must be a HTML/CSS/JS frontend that accesses data from a public API. All interactions between the client and the API should be handled asynchronously and use JSON as the communication format.
+-https://www.swapi.tech/api
+
+Your entire app must run on a single page. There should be NO redirects. In other words, your project will contain a single HTML file.
+
+Some interactivity is required. This interactivity needs to incorporate at least 3 separate event listeners. This could be as simple as adding a "like" button or adding comments. These interactions do not need to persist after reloading the page.
+
+-Event Listener 1:
+User clicks on something from the dropdown menu to retrieve and display information about:
+Films: more specifically details about each film
+
+Event Listener 2:
+User Decides whether they "like" the film or not using a checkbox
+
+Event Listener 3:
+User clicks a "Start Over" button to clear screen and start over
--- index.js
+++ index.js
@@ -1,70 +1,15 @@
 console.log ("Hi Star Wars Fans.  You can't see this because it's a console.log.")
 
-//Event Listener 1 - hit a button and Fetches from Star Wars API https://www.swapi.tech/
-document.getElementById("planets").addEventListener("click", fetchPlanet);
-function fetchPlanet() {
-    fetch('https://www.swapi.tech/api/planets/')
-    .then(res => res.json())
-    .then(data => console.log(data.results))
-    .catch(err => console.error(err)
-    )
-  }
-
-document.getElementById("starships").addEventListener("click", fetchShip);
-function fetchShip() {
-    fetch('https://www.swapi.tech/api/starships/')
-    .then(res => res.json())
-    .then(data => console.log(data.results))
-    .catch(err => console.error(err)
-    )
-  }
-
-document.getElementById("vehicles").addEventListener("click", fetchVehicles);
-function fetchVehicles() {
-    fetch('https://www.swapi.tech/api/vehicles/')
-    .then(res => res.json())
-    .then(data => console.log(data.results))
-    .catch(err => console.error(err)
-    )
-  }
-
-document.getElementById("people").addEventListener("click", fetchPeople);
-function fetchPeople() {
-    fetch('https://www.swapi.tech/api/people/')
-    .then(res => res.json())
-    .then(data => console.log(data.results))
-    .catch(err => console.error(err)
-    )
-  }
-
-document.getElementById("films").addEventListener("click", fetchFilms);
-function fetchFilms() {
-    fetch('https://www.swapi.tech/api/films/')
-    .then(res => res.json())
-    .then(data => console.log(data.result))
-    .catch(err => console.error(err))
-    
-  }
-
-document.getElementById("species").addEventListener("click", fetchSpecies);
-function fetchSpecies() {
-    fetch('https://www.swapi.tech/api/species/')
-    .then(res => res.json())
-    .then(data => console.log(data.results))
-    .catch(err => console.error(err)
-    )
-  }
-
-
+//Event Listener 1 - hit a button and Fetches from Star Wars API https://www.swapi.tech/api
 // These are the fetch requests for the "FILMS" dropdown menu
 document.getElementById("4").addEventListener("click", fetchEpisodeIV);
 function fetchEpisodeIV() {
   fetch("https://www.swapi.tech/api/films/1/")
-.then(res => res.json())
-.then(data => console.log(data.result.properties))
-.catch(err => console.error(err))
-      
-    }
+  .then(res => res.json())
+  .then(data => console.log(data.result.properties))
+    .catch(err => console.error(err))
+    
+}
 
 document.getElementById("5").addEventListener("click", fetchEpisodeV);
 function fetchEpisodeV() {
@@ -110,13 +55,18 @@ function fetchEpisodeIII() {
   .catch(err => console.error(err))
             
 }
-        
-      
-      
-// display info about selected random content on screen 
+ 
+// display info about selected content on screen 
+// make a div and make a card
+
 
 //Event Listener 2 - user can click the heart button and the information about that item/movie will save down below
 
-//Event Listener 3 - delete option for saved info
+//Event Listener 3 - 
+
+document.getElementById("refresh-btn").addEventListener("click", startOver);
+function startOver(){
+  location.reload()
+}
 
 //stretch goal - add to db.json server to save the favorited characters?
