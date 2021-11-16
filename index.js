47a08c96e8fb69e24740380ee451c725016dbd4a
joshua david jennings <jennings.josh@gmail.com>
2021-11-16 14:25:26 -0600

    Reordered Js file and html file

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const cardContainer = document.querySelector('#card-container')
-//Event Listener 1 - hit a button and Fetches from Star Wars API https://www.swapi.tech/api
-// These are the fetch requests for the "FILMS" dropdown menu
+
 document.getElementById("movie-select").addEventListener("change", fetchEpisodeData);
 document.getElementById("comment-btn").addEventListener("click", postComment)
 
+//Yoda Refresh button 
+document.getElementById("refresh-btn").addEventListener("click", startOver);
+function startOver(){
+  location.reload()
+}
+
+function fetchEpisodeData(event) {
+  fetch(`https://www.swapi.tech/api/films/${event.target.value}/`)
+  .then(res => res.json())
+  .then(data => displayInfo(data.result.properties)
+  
+  )}
+ 
+  //displayInfo function
+function displayInfo(movieDetails){
+  // cardContainer.innerHTML =  "";
+  cardContainer.innerHTML = `<div class="card mb-4" style="width: 18rem; margin: auto;">
+  <div class="card-body">
+    <h5 class="card-title">${movieDetails.title}</h5>
+    <p class="card-text">Episode: ${movieDetails.episode_id}</p>
+    <p class="card-text">Release Date: ${movieDetails.release_date}</p>
+    <p class="card-text">Director: ${movieDetails.director}</p>
+  </div>
+</div>`
+
+  }
+// Comment section
 function postComment() {
   const commentContainer = document.getElementById("comments-container");
   const commentInput = document.getElementById("comment-input");
@@ -18,29 +44,4 @@ function postComment() {
   }
 }
 
-function fetchEpisodeData(event) {
-  fetch(`https://www.swapi.tech/api/films/${event.target.value}/`)
-  .then(res => res.json())
-  .then(data => displayInfo(data.result.properties)
-  
-  )}
- 
-  //displayInfo function
-function displayInfo(movieDetails){
-  cardContainer.innerHTML =  "";
-  cardContainer.innerHTML = `<div class="card mb-4" style="width: 18rem; margin: auto;">
-  <div class="card-body">
-    <h5 class="card-title">${movieDetails.title}</h5>
-    <p class="card-text">Episode: ${movieDetails.episode_id}</p>
-    <p class="card-text">Release Date: ${movieDetails.release_date}</p>
-    <p class="card-text">Director: ${movieDetails.director}</p>
-  </div>
-</div>`
 
-  }
-
-//Yoda Refresh button 
-document.getElementById("refresh-btn").addEventListener("click", startOver);
-function startOver(){
-  location.reload()
-}
