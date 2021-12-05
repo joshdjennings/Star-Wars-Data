4e3c3d5ef069468d19cd54e0e14d832e702a502e
joshua david jennings <jennings.josh@gmail.com>
2021-12-05 16:45:52 -0600

    cleaned up formatting

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 # Star-Wars-Data
+
 Event Listener 1:
 User clicks on a movie from the dropdown menu to retrieve and display information about:
 thei selected movie. Details about each film will be displayed in a card on screen.
 
 Event Listener 2:
-User clicks on Yoda's quote to clear the card.  The User can then choose a different movie.
+User clicks on Yoda's quote to clear the card. The User can then choose a different movie.
 
 Event Listener 3:
-User can post a comment and see it displayed on screen after they "Post" it.  Comments will remain even if users cycle through different movies' information.
-
-
-
-
+User can post a comment and see it displayed on screen after they "Post" it. Comments will remain even if users cycle through different movies' information.
--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const cardContainer = document.querySelector('#card-container')
-const commentContainer = document.querySelector('#comment-container')
+const cardContainer = document.querySelector('#card-container');
+const commentContainer = document.querySelector('#comment-container');
 
-document.getElementById("movie-select").addEventListener("change", fetchEpisodeData);
-document.getElementById("refresh-btn").addEventListener("click", startOver);
+document
+	.getElementById('movie-select')
+	.addEventListener('change', fetchEpisodeData);
+document.getElementById('refresh-btn').addEventListener('click', startOver);
 document.getElementById('form').addEventListener('submit', commentPost);
 
 function fetchEpisodeData(event) {
-  fetch(`https://www.swapi.tech/api/films/${event.target.value}/`)
-  .then(res => res.json())
-  .then(data => displayInfo(data.result.properties))
-  } 
- 
-function displayInfo(movieDetails){
-    cardContainer.innerHTML = `<div class="card mb-4" style="width: 50rem; margin: auto;">
+	fetch(`https://www.swapi.tech/api/films/${event.target.value}/`)
+		.then((res) => res.json())
+		.then((data) => displayInfo(data.result.properties));
+}
+
+function displayInfo(movieDetails) {
+	cardContainer.innerHTML = `<div class="card mb-4" style="width: 50rem; margin: auto;">
     <div class="card-body">
     <img class="card-img-top" src="https://cdn.europosters.eu/image/750/posters/star-wars-complete-saga-i19317.jpg" alt="Card image cap">
     
@@ -23,45 +25,41 @@ function displayInfo(movieDetails){
       <p class="card-text">${movieDetails.opening_crawl}</p>
       
     </div>
-  </div>`;
-  }
+    </div>`;
+}
 
-function startOver(){
-  document.getElementById("movie-select").value = '';
-  cardContainer.innerText = '';
-  commentContainer.innerText = '';
-  }
+function startOver() {
+	document.getElementById('movie-select').value = '';
+	cardContainer.innerText = '';
+	commentContainer.innerText = '';
+}
 
 function commentPost(event) {
-  event.preventDefault();
-  const formInput = document.getElementById("form-input");
-  const formText = formInput.value;
-  commentContainer.innerHTML = `<div class="card mb-4" style="width: 50rem; margin: auto;">
-    <div class="card-body">
-      <p>${formText}</p>
-    </div>
-  </div>`;
-  document.getElementById("form-input").value = '';
-  // document.createElement("div")
-  // document.commentContainer.append(`${formText}`)
-
-console.log(div.textContent)
+	event.preventDefault();
+	const formInput = document.getElementById('form-input');
+	const formText = formInput.value;
+	commentContainer.innerHTML = `<div class="card mb-4" style="width: 50rem; margin: auto;">
+        <div class="card-body">
+           <p>${formText}</p>
+        </div>
+        </div>`;
+	document.getElementById('form-input').value = '';
 }
 
 const numStars = 100;
 for (let i = 0; i < numStars; i++) {
-  let star = document.createElement("div");  
-  star.className = "star";
-  var xy = getRandomPosition();
-    star.style.top = xy[0] + 'px';
-    star.style.left = xy[1] + 'px';
-  document.body.append(star);
+	let star = document.createElement('div');
+	star.className = 'star';
+	var xy = getRandomPosition();
+	star.style.top = xy[0] + 'px';
+	star.style.left = xy[1] + 'px';
+	document.body.append(star);
 }
 
-function getRandomPosition() {  
-  var y = window.innerWidth;
-  var x = window.innerHeight;
-    var randomX = Math.floor(Math.random()*x);
-    var randomY = Math.floor(Math.random()*y);
-    return [randomX,randomY];
+function getRandomPosition() {
+	var y = window.innerWidth;
+	var x = window.innerHeight;
+	var randomX = Math.floor(Math.random() * x);
+	var randomY = Math.floor(Math.random() * y);
+	return [randomX, randomY];
 }
